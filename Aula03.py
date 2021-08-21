5a0165b9c9af4122ddc4ff533101db6bd1733540
Roberto Abreu <abreuroberto2015@gmail.com>
2021-08-21 16:57:07 -0300

    21/08

---
+++ Aula03.py
@@ -0,0 +1,7 @@
+# interagindo com o Usuario - Input
+
+nome = input("Digite o seu nome: ")
+
+idade = input("Digite sua idade:")
+
+print("Oi " + nome + ", sua idade é " + idade)
---
+++ Aula04.py
@@ -0,0 +1,7 @@
+num1 = input("Digite o primeiro numero: ")
+num2 = input("Digite o segundo numero: ")
+
+soma = float(num1) + float(num2)
+#subtrai = num1 - num2
+
+print("O resultado sa soma dos 2 numeros é: " + str(soma))
